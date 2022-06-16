144a89fab3adb27fa58666a281b1fa9e63908972
Carlos <candreynascimento@gmail.com>
2022-06-16 15:14:33 -0300

    feat: tela principal do usuário, faltando implementar estilo e alguns botões

---
+++ src/pages/main/index.js
@@ -0,0 +1,27 @@
+import { View, Text, Pressable } from 'react-native'
+import React from 'react'
+import style from './style'
+import Card from '../../components/cards'
+
+export default function Main({ navigation }) {
+	const goToRegisterGlucose = () => {
+		navigation.navigate('registerGlucose')
+	}
+	return (
+		<View style={style.container}>
+			<Text></Text>
+			<View style={style.carrousel}>
+				<Card></Card>
+				<Card></Card>
+
+			</View>
+			<View style={style.buttons}>
+
+				<Pressable style={{ ...style.button, ...style.defaultButton }}
+					onPress={goToRegisterGlucose}>
+					<Text style={{ color: 'white', fontWeight: 'bold', ...style.fontSizeButton }}>Registrar refeição</Text>
+				</Pressable>
+			</View>
+		</View>
+	)
+}
