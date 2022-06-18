9293d5b9bc4012f8e81137369394db24114cbe6e
LUCAS ARAUJO EMMERICH <18969750703@cefet-rj.br>
2022-06-18 01:28:40 -0300

    adds first part of dose calculator page

--- src/pages/dose-calculator/style.js
+++ src/pages/dose-calculator/style.js
@@ -37,5 +37,65 @@ export default StyleSheet.create({
 	font: {
 		fontFamily: 'Montserrat',
 	},
-
+	searchableDropDown:{
+		containerStyle: {
+			width: '85%',
+			alignSelf: 'center',
+		},
+		textInputStyle: {
+			borderWidth: 1,
+			borderStyle: 'solid',
+			borderColor: '#C0C0C0',
+			borderRadius: 8,
+			height: 45,
+			paddingLeft: 10,
+		},
+		itemStyle: {
+			padding: 10,
+			marginTop: 2,
+			backgroundColor: '#FAF9F8',
+			borderColor: '#bbb',
+			borderWidth: 1,
+		}
+	},
+	dosageCard: {
+		height: 150,
+		borderRadius: 8,
+		padding: 10,
+		paddingTop: 0,
+		paddingRight: 0,
+		width: '85%',
+		alignItems: 'center',
+		alignSelf: 'center',
+		backgroundColor: '#d8f2f2',
+		marginTop: 20,
+		borderWidth: 0.01,
+		shadowColor: "#000",
+		shadowOffset: {
+			width: 0,
+			height: 12,
+		},
+		shadowOpacity: 0.58,
+		shadowRadius: 16.00,
+		elevation: 24
+	},
+	dosageRow: { 
+		flexDirection: 'row', 
+		alignItems: 'center', 
+		justifyContent: 'center'
+	},
+	dosageLabel: {
+		padding: 5,
+		fontWeight: 'bold'
+	},
+	bestDose:{
+		fontWeight: '900',
+		fontSize: 20,
+		backgroundColor: '#05fa2a',
+		width: 150,
+		marginRight: 0,
+		marginLeft: 'auto',
+		color: 'white',
+		borderTopRightRadius: 8
+	}
 })
